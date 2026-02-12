1eab1f7bff1159d4f489cbef5d6c34cdf3504108
sagarms2005 <2005sagarms@gmail.com>
2026-02-12 10:27:50 +0530

    Added methouds 1st  programs

---
+++ FacebookSignup.java
@@ -0,0 +1,21 @@
+package jan30.task;
+
+public class FacebookSignup {
+    public static void facebookLogin(){
+        String firstName = "Sagar";
+        String surName = "M S";
+        String contactInfo = "2005sagarms@gmail.com";
+        String pass = "Sagu@123";
+        byte birthDay = 14;
+        String birthMonth = "May";
+        short birthYear = 2005;
+        char gender = 'M';
+
+        System.out.println("First Name: " + firstName);
+        System.out.println("Surname: " + surName);
+        System.out.println("Email/Mobile: " + contactInfo);
+        System.out.println("Password: " + pass);
+        System.out.println("DOB: " + birthDay + "/" + birthMonth + "/" + birthYear);
+        System.out.println("Gender: " + gender);
+    }
+}
---
+++ SpotifyProfile.java
@@ -0,0 +1,17 @@
+package jan30.task;
+
+public class SpotifyProfile {
+    public static void login() {
+        String mail = "sagar@spotify.com";
+        String password = "musicLover123";
+        String nickname = "Sagar Beats";
+        int yob = 2005;
+        char sex = 'M';
+
+        System.out.println("User Email: " + mail);
+        System.out.println("Password: " + password);
+        System.out.println("Profile: " + nickname);
+        System.out.println("Birth Year: " + yob);
+        System.out.println("Gender Code: " + sex);
+    }
+}
